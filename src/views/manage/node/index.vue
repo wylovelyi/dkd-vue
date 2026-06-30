3e5e4c40c4a2ddf06cc8e177660c45f1eb5921b4
unknown <2016688030@qq.com>
2026-06-30 14:31:51 +0800

    fix: 修复点位管理页面 loadAllParams 重复声明编译错误

--- src/views/manage/node/index.vue
+++ src/views/manage/node/index.vue
@@ -9,7 +9,7 @@
           @keyup.enter="handleQuery"
         />
       </el-form-item>
-      <el-form-item label="区域ID" prop="regionId">
+      <el-form-item label="区域搜索" prop="regionId">
         <!-- <el-input
           v-model="queryParams.regionId"
           placeholder="请输入区域ID"
@@ -75,21 +75,21 @@
     </el-row>
 
     <el-table v-loading="loading" :data="nodeList" @selection-change="handleSelectionChange">
-      <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="主键id" align="center" prop="id" />
+      <el-table-column type="selection" width="50" align="center" />
+      <el-table-column label="序号" type="index" width=50 align="center" prop="id" />
       <el-table-column label="点位名称" align="center" prop="nodeName" />
-      <el-table-column label="详细地址" align="center" prop="address" />
+      <el-table-column label="区域ID" align="center" prop="regionId" />
       <el-table-column label="商圈类型" align="center" prop="businessType">
         <template #default="scope">
           <dict-tag :options="business_type" :value="scope.row.businessType"/>
         </template>
       </el-table-column>
-      <el-table-column label="区域ID" align="center" prop="regionId" />
       <el-table-column label="合作商ID" align="center" prop="partnerId" />
+      <el-table-column label="详细地址" align="center" prop="address" show-overflow-tooltip />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['manage:node:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['manage:node:remove']">删除</el-button>
+          <el-button link type="primary"  @click="handleUpdate(scope.row)" v-hasPermi="['manage:node:edit']">修改</el-button>
+          <el-button link type="primary"  @click="handleDelete(scope.row)" v-hasPermi="['manage:node:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -108,8 +108,18 @@
         <el-form-item label="点位名称" prop="nodeName">
           <el-input v-model="form.nodeName" placeholder="请输入点位名称" />
         </el-form-item>
-        <el-form-item label="详细地址" prop="address">
-          <el-input v-model="form.address" type="textarea" placeholder="请输入内容" />
+        <el-form-item label="所属区域" prop="regionId">
+        
+          <!--<el-input v-model="form.regionId" placeholder="请输入区域ID" />-->
+          
+          <el-select v-model="form.regionId" placeholder="请选择区域ID">
+            <el-option
+              v-for="item in regionList"
+              :key="item.id"
+              :label="item.regionName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="商圈类型" prop="businessType">
           <el-select v-model="form.businessType" placeholder="请选择商圈类型">
@@ -121,11 +131,20 @@
             ></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="区域ID" prop="regionId">
-          <el-input v-model="form.regionId" placeholder="请输入区域ID" />
-        </el-form-item>
+
         <el-form-item label="合作商ID" prop="partnerId">
-          <el-input v-model="form.partnerId" placeholder="请输入合作商ID" />
+          <!--<el-input v-model="form.partnerId" placeholder="请输入合作商ID" />-->
+          <el-select v-model="form.partnerId" placeholder="请选择合作商ID"> 
+          <el-option
+              v-for="item in partnerList"
+              :key="item.id"
+              :label="item.partnerName"
+              :value="item.id"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+                <el-form-item label="详细地址" prop="address">
+          <el-input v-model="form.address" type="textarea" placeholder="请输入内容" />
         </el-form-item>
       </el-form>
       <template #footer>
@@ -141,6 +160,8 @@
 <script setup name="Node">
 import { listNode, getNode, delNode, addNode, updateNode } from "@/api/manage/node";
 import {listRegion} from "@/api/manage/region";
+import {listPartner} from "@/api/manage/partner";
+import {loadAllParams} from "@/api/page";
 
 const { proxy } = getCurrentInstance();
 const { business_type } = proxy.useDict('business_type');
@@ -297,12 +318,6 @@ function handleExport() {
 /** 区域列表 */
 const regionList = ref([]);
 
-/** 查询所有的条件对象 */
-const loadAllParams = reactive({
-  pageNum: 1,
-  pageSize: 10000
-});
-
 /**
  * 查询区域列表
  */
@@ -311,8 +326,15 @@ function getRegionList() {
     regionList.value = response.rows;
   });
 }
-
-getList();
+/** 查询合作商列表 */
+const partnerList = ref([]);
+function getPartnerList() {
+  listPartner(loadAllParams).then(response => {
+    partnerList.value = response.rows;
+  });
+}
+getPartnerList();
 getRegionList();
+getList();
 </script>
 
